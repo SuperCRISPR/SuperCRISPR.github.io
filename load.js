485bd16e4555924603d6af61fc9d4629819a931a
SuperCRISPR <2696331468@qq.com>
2022-07-09 08:36:47 +0800

    2022/7/9

---
+++ load.js
@@ -0,0 +1,9 @@
+$(function autoload() {
+    // $('#selector').load(url,data,function(response,status,xhr))
+    $('#navbar_HOME').load('/base/navbar_HOME.html');
+
+});
+
+//usage:
+//<in HTML>
+//<div id="selector"> </div>
