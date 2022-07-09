b3532f58facd3f2ecdfa866739197dcead4245a6
SuperCRISPR <2696331468@qq.com>
2022-07-09 13:23:19 +0800

    2022/7/9-2nd

--- load.js
+++ load.js
@@ -1,6 +1,9 @@
 $(function autoload() {
     // $('#selector').load(url,data,function(response,status,xhr))
     $('#navbar_HOME').load('/base/navbar_HOME.html');
+    $('#navbar_MyLife').load('/base/navbar_MyLife.html');
+    $('#navbar_AboutMe').load('/base/navbar_AboutMe.html');
+    $('#navbar_TreasureHouse').load('/base/navbar_TreasureHouse.html');
 
 });
 
