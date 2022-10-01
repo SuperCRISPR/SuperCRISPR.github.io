987d6453f1208e7514ddcb7cf3aebfaa49720fea
SuperCRISPR <2696331468@qq.com>
2022-10-02 01:33:49 +0800

    0

---
+++ load.js
@@ -0,0 +1,13 @@
+$(function autoload() {
+    // $('#selector').load(url,data,function(response,status,xhr))
+    $('#navbar_HOME').load('../../base/navbar_HOME.html');
+    $('#navbar_MyLife').load('../../base/navbar_MyLife.html');
+    $('#navbar_AboutMe').load('../../base/navbar_AboutMe.html');
+    $('#navbar_TreasureHouse').load('../../base/navbar_TreasureHouse.html');
+    $('#footer').load('../../base/footer.html');
+
+});
+
+//usage:
+//<in HTML>
+//<div id="selector"> </div>
